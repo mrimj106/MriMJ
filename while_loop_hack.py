050eb5e1ff990ce30f506cf54e37e2d2168ac8f9
Mrityunjay Singh <54023332+mrimj106@users.noreply.github.com>
2019-08-19 23:47:01 +0530

    Add files via upload

---
+++ while_loop_hack.py
@@ -0,0 +1,9 @@
+ages={'dad':42,'mom':48,'lisa':7}
+while 20:
+    print "Enter quite Anytime, when you want to exit"
+    name= raw_input("enter the name: ")
+    if name == "quite": break
+while 1:
+    print "Balidaan Dena Hoga Balidaan Dena Hoga Balidaan Dena Hoga Balidaan Dena Hoga"
+
+
