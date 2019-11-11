b5b90b023310dcea357df4d50b47e90599a119a6
Mrityunjay Singh <54023332+mrimj106@users.noreply.github.com>
2019-11-12 02:09:50 +0530

    Check file existance
    #This program checks if the file exists or not using os.path.isfile() method
    #If file exists it will return True
    #If file do not exists it will return False

---
+++ check_file_exits.py
@@ -0,0 +1,11 @@
+#Program to check if file exists or not
+
+import os
+f1=os.path.isfile("C:/Users/mritsing/Desktop/Python/test1/test2/log1.txt")
+if f1==True:
+    print ("File exists")
+else:
+    print ("File does not exists")
+
+
+
