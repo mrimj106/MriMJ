24fb3e76b8ca14398a3704c723a7d7d03dc45c5f
Mrityunjay Singh <54023332+mrimj106@users.noreply.github.com>
2019-11-12 02:03:04 +0530

    Check whether the path exists or not
    #This program checks if the path exists or not using os.path.exists method
    #If the file exists then it will return value as true
    #If the file do not exists it will return value as false

---
+++ check_path_exits.py
@@ -0,0 +1,11 @@
+#Program to check if path exists or not
+
+import os
+f1=os.path.exists("C:/Users/mritsing/Desktop/Python/test1/test3")
+if f1==True:
+    print ("path exists")
+else:
+    print ("path does not exists")
+
+
+
