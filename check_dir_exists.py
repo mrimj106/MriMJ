b9f31ca34d56b2fbecae2aaf16296b767f79fbe9
Mrityunjay Singh <54023332+mrimj106@users.noreply.github.com>
2019-11-12 02:14:23 +0530

    Check dir exists
    #This program checks if the specified directory exists or not
    #If the specified directory exists it will return true
    #If the specified directory do not exists it will return False

---
+++ check_dir_exists.py
@@ -0,0 +1,11 @@
+#Program to check if Directory exists or not
+
+import os
+f1=os.path.isdir("C:/Users/mritsing/Desktop/Python/test1/test2")
+if f1==True:
+    print ("Directory exists")
+else:
+    print ("Directory does not exists")
+
+
+
