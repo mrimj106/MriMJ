6790d7da20d69677f1a7f33fa5608db193ef893d
Mrityunjay Singh <54023332+mrimj106@users.noreply.github.com>
2019-12-15 22:40:23 +0530

    Update check_file_exits.py

--- check_file_exits.py
+++ check_file_exits.py
@@ -3,7 +3,7 @@
 import os
 f1=os.path.isfile("C:/Users/mritsing/Desktop/Python/test1/test2/log1.txt")
 if f1==True:
-    print ("File exists")
+    print ("File exists") #This will check if the file exists or not
 else:
     print ("File does not exists")
 
